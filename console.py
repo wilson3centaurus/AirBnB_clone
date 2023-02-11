4835fb30b81bdb2d7f3112fcf3e37eddc868cd47
EMMANUEL-ALX <emmanuelanaedobe@gmail.com>
2023-02-11 06:18:17 -0800

    upd-console

--- console.py
+++ console.py
@@ -2,7 +2,6 @@
 """ This module defines the (hbnb) console """
 import cmd
 from models.base_model import BaseModel
-from models import storage
 from models.user import User
 from models.state import State
 from models.city import City
