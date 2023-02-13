5d36696b28dd53846e5f3d88b7ab675157986f7e
EMMANUEL-ALX <emmanuelanaedobe@gmail.com>
2023-02-12 18:18:53 -0800

    updated

--- models/state.py
+++ models/state.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-""" This module defines the class `State` """
+"""This module creates a User class"""
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """ Represents the location (State)
-
-    Attributes:
-        name (str): name of the State
-    """
+    """Class for managing state objects"""
 
     name = ""
