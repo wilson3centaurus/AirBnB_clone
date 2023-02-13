92e80eb5ed11be3e4206dbd365b2026580318ffd
EMMANUEL-ALX <emmanuelanaedobe@gmail.com>
2023-02-12 18:07:38 -0800

    updated

--- models/city.py
+++ models/city.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-""" This module defines the class `City` """
+"""This module creates a User class"""
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """ Represents the location (City)
-
-    Attributes:
-        name (str): name of the City
-        state_id (str): the id of the state
-    """
+    """Class for managing city objects"""
 
     state_id = ""
     name = ""
