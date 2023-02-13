6085c8072e1ff018830bc061f67741f9ec1b87c2
EMMANUEL-ALX <emmanuelanaedobe@gmail.com>
2023-02-12 18:11:50 -0800

    updated

--- models/place.py
+++ models/place.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-""" This module defines the class `Place` """
+"""This module creates a Place class"""
+
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Represent an AirBnB
-    Attributes:
-        city_id (str): The City id
-        user_id (str): The User id
-        name (str): The name of the place
-        description (str): The description of the place
-        number_rooms (int): The number of rooms
-        number_bathrooms (int): The number of bathrooms
-        max_guest (int): The maximum number of guests allowed
-        price_by_night (int): The price by night
-        latitude (float): The latitude of the place
-        longitude (float): The longitude of the place
-        amenity_ids (list): A list of Amenity ids
-    """
+    """Class for managing place objects"""
 
     city_id = ""
     user_id = ""
