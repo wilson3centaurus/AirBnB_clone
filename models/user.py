d4fd545ea55bf6f922515a21d15e6f8b4bb1814c
EMMANUEL-ALX <emmanuelanaedobe@gmail.com>
2023-02-12 18:22:18 -0800

    updated

--- models/user.py
+++ models/user.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-""" This module defines the class `User` """
+"""This module creates a User class"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ Creates a new User
-
-    Attributes:
-        email (str): user's email
-        password (str): user's password
-        first_name (str): first name of the user
-        last_name (str): last name of the user
-    """
+    """Class for managing user objects"""
 
     email = ""
     password = ""
